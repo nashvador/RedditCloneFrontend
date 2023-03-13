2e54648ba9ff54ef391ce1331c14c8d3d3296e34
nashvador <nashvador@gmail.com>
2023-03-13 09:00:03 -0400

    Fix sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Fragment} from 'react';
 import './App.css';
 import { Header } from './Header/header';
+import { Grid } from '@mui/material';
 
 function App() {
   return (
--- src/Header/header.tsx
+++ src/Header/header.tsx
@@ -1,15 +1,15 @@
 import React, { Fragment} from "react";
 import { Button } from "@mui/material";
 import PublicIcon from '@mui/icons-material/Public'; 
-import './header.css'
-
 
 const Header = () => {
     return (
         <Fragment>
-            <Button id="getRedButton">
-                Get New Reddit
-                <PublicIcon id="worldSymbol" />
+            <Button>
+                Log in
+            </Button>
+            <Button>
+                Sign up
             </Button>
         </Fragment>
     )
